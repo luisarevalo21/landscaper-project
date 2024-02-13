10aa9167cf881cdfa4e508410a30279ea15e7e44
luisarevalo21 <arevaloluis89@yahoo.com>
2024-02-12 16:58:17 -0800

    moved carousel into own component

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,81 @@
+import Carousel from "react-bootstrap/Carousel";
+import Image from "react-bootstrap/Image";
+
+import React from "react";
+
+const CarouselComponent = ({ images, showButtons, seperator }) => {
+  console.log("carousel called", images);
+  const items = images.map((image, index) => (
+    <Carousel.Item key={index}>
+      <Image src={image.image} className="carousel__image" />
+      <Carousel.Caption className="text-center carousel__caption__container">
+        <h3 className="carousel__toptext carousel__toptext--font">{image.mainText}</h3>
+        {seperator && <div className="carousel__separator"></div>}
+        <p className="carousel__subtext carousel__subtext--font">{image.subText}</p>
+        {showButtons && (
+          <div className="carousel__btn__container">
+            <button className="carousel__btn carousel__btn--green carousel__btn--font carousel__btn--padding">
+              Get in Touch
+            </button>
+            <button className="carousel__btn carousel__btn--clear carousel__btn--font carousel__btn--padding">
+              Learn More
+            </button>
+          </div>
+        )}
+      </Carousel.Caption>
+    </Carousel.Item>
+  ));
+
+  return <Carousel fade>{items}</Carousel>;
+};
+
+{
+  /* <Carousel.Item>
+        <Image src="https://themedemo.foundstrap.com/zengarden/img/services/service-6.jpg" className="hero__image" />
+        <Carousel.Caption className="text-center">
+          <h3 className="service__title">Watering and Irrigation</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image
+          src="https://themedemo.foundstrap.com/zengarden/img/services/service-4.jpg"
+          className="hero__image"
+          alt="slider image"
+        />
+        <Carousel.Caption>
+          <h3 className="service__title">Garden Clearance</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image
+          src="https://themedemo.foundstrap.com/zengarden/img/services/service-5.jpg"
+          className="hero__image"
+          alt="slider image"
+        />
+        <Carousel.Caption>
+          <h3 className="service__title">Stone hardscaping </h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image
+          src="https://themedemo.foundstrap.com/zengarden/img/services/service-2.jpg"
+          className="hero__image"
+          alt="slider image"
+        />
+        <Carousel.Caption>
+          <h3 className="service__title"> Decoration and landscaping</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image
+          src="https://themedemo.foundstrap.com/zengarden/img/services/service-1.jpg"
+          className="hero__image"
+          alt="slider image"
+        />
+        <Carousel.Caption>
+          <h3 className="service__title">Lawn and Garden Maintenance</h3>
+        </Carousel.Caption>
+      </Carousel.Item> */
+}
+
+export default CarouselComponent;
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,65 +1,39 @@
-import React from "react";
-import Carousel from "react-bootstrap/Carousel";
+import React, { useState } from "react";
+import Carousel from "./Carousel";
 import heroImage1 from "../assets/hero_image_1.jpg";
 import heroImage2 from "../assets/hero_image_2.jpg";
 import heroImage3 from "../assets/hero_image_3.jpg";
 import Image from "react-bootstrap/Image";
 import Card from "./Card";
+
 const Services = () => {
+  const [images, setImages] = React.useState([
+    {
+      image: "https://themedemo.foundstrap.com/zengarden/img/services/service-6.jpg",
+      subText: "Watering and Irrigation",
+    },
+    {
+      image: "https://themedemo.foundstrap.com/zengarden/img/services/service-4.jpg",
+      subText: "Garden Clearance",
+    },
+    {
+      image: "https://themedemo.foundstrap.com/zengarden/img/services/service-5.jpg",
+      subText: "Stone hardscaping",
+    },
+    {
+      image: "https://themedemo.foundstrap.com/zengarden/img/services/service-2.jpg",
+      subText: "Decoration and landscaping",
+    },
+    {
+      image: "https://themedemo.foundstrap.com/zengarden/img/services/service-1.jpg",
+      subText: "Lawn and Garden Maintenance",
+    },
+  ]);
   return (
     <div className="services">
       <div className="services__photos">
-        <Carousel fade>
-          <Carousel.Item>
-            <Image
-              src="https://themedemo.foundstrap.com/zengarden/img/services/service-6.jpg"
-              className="hero__image"
-            />
-            <Carousel.Caption className="text-center">
-              <h3 className="service__title">Watering and Irrigation</h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <Image
-              src="https://themedemo.foundstrap.com/zengarden/img/services/service-4.jpg"
-              className="hero__image"
-              alt="slider image"
-            />
-            <Carousel.Caption>
-              <h3 className="service__title">Garden Clearance</h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <Image
-              src="https://themedemo.foundstrap.com/zengarden/img/services/service-5.jpg"
-              className="hero__image"
-              alt="slider image"
-            />
-            <Carousel.Caption>
-              <h3 className="service__title">Stone hardscaping </h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <Image
-              src="https://themedemo.foundstrap.com/zengarden/img/services/service-2.jpg"
-              className="hero__image"
-              alt="slider image"
-            />
-            <Carousel.Caption>
-              <h3 className="service__title"> Decoration and landscaping</h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <Image
-              src="https://themedemo.foundstrap.com/zengarden/img/services/service-1.jpg"
-              className="hero__image"
-              alt="slider image"
-            />
-            <Carousel.Caption>
-              <h3 className="service__title">Lawn and Garden Maintenance</h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+        <div> test</div>
+        <Carousel images={images} seperator={false} />
       </div>
 
       <div className="services__details">
