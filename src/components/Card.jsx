79db6a4b6eeeaf901ccbd9cf90df75a65ec527fc
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-02-14 11:44:58 -0800

    Style service cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,10 +13,8 @@ const Card = ({ title, description, service, image }) => {
   }
   return (
     <div className="card">
-      <div className="card__details">
-        <h2 className="card__header">{title}</h2>
-        <p className="card__description">{description}</p>
-      </div>
+      <h2 className="card__header">{title}</h2>
+      <p className="card__description">{description}</p>
       <span className="material-symbols-outlined card__icon">cycle</span>
     </div>
   );
