0f5feca95be98d2b43d4e5bc150da73db27d5edc
luisarevalo21 <arevaloluis89@yahoo.com>
2024-02-17 18:21:09 -0800

    added google maps above footer on all pages

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -17,8 +17,8 @@ const ContactPage = () => {
           </h2>
           <p className="info__para">
             If you have any question about Zengarden or how we can help to maintenance your garden, please contact us
-            directly by selecting from one of the below categories that is the most relevant to your inquiry and fill our
-            short feedback form.
+            directly by selecting from one of the below categories that is the most relevant to your inquiry and fill
+            our short feedback form.
           </p>
         </div>
         <div className="contact__form__container">
@@ -44,7 +44,15 @@ const ContactPage = () => {
             <p className="contact__item__text">Spanish: (510)-860-9831</p>
           </div>
         </div>
-        <div>(google map goes here)</div>
+        {/* <div>(google map goes here)</div> */}
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d201212.43130781694!2d-122.47703890803662!3d38.00176107762278!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808577c47504c10d%3A0x360b82bd722d1f2d!2sGreen%20lawn%20services!5e0!3m2!1sen!2sus!4v1708222682619!5m2!1sen!2sus"
+          width="600"
+          height="450"
+          className="contact__map"
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
       </div>
 
       <Footer />
